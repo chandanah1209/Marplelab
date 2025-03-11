bda974c9d5d897446b38f6f8346d20a04eb913b1
chandanah1209 <chandanah1209@gmail.com>
2025-03-11 15:54:37 +0000

    successfully committed

---
+++ myproject/app.py
@@ -0,0 +1,28 @@
+from flask import Flask
+import subprocess
+import os
+import datetime
+
+app = Flask(_name_)
+
+@app.route('/htop')
+def htop():
+    name = "Your Full Name"
+    username = os.getlogin()
+    server_time = datetime.datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+    top_output = subprocess.getoutput("top -b -n 1")
+
+    response = f"""
+    <pre>
+    Name: {name}
+    User: {username}
+    Server Time (IST): {server_time}
+
+    TOP output:
+    {top_output}
+    </pre>
+    """
+    return response
+
+if _name_ == "_main_":
+    app.run(host="0.0.0.0", port=5000, debug=True)
